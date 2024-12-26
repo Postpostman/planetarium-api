3e37a179bc2f3ec78558ceefe64f56409f1ec2f9
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2024-12-27 01:49:07 +0200

    register models into admin

--- planetarium/admin.py
+++ planetarium/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from planetarium.models import (
+    ShowSession,
+    ShowTheme,
+    Reservation,
+    AstronomyShow,
+    PlanetariumDome,
+    Ticket)
+
+admin.site.register(ShowSession)
+admin.site.register(ShowTheme)
+admin.site.register(Ticket)
+admin.site.register(Reservation)
+admin.site.register(PlanetariumDome)
+admin.site.register(AstronomyShow)
+
