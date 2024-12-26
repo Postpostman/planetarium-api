fa795bd791b36b92bb56a01a2a7ec8f618a0cf00
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2024-12-26 17:12:03 +0200

    update models and make migrations

--- planetarium/models.py
+++ planetarium/models.py
@@ -1,54 +1,129 @@
+import os
+import uuid
+
+from django.core.exceptions import ValidationError
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
+from django.utils.text import slugify
+
+
+class PlanetariumDome(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+    def __str__(self):
+        return self.name
+
+
+class ShowTheme(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+def astronomy_show_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+    return os.path.join("uploads/astronomy_shows/", filename)
 
 
 class AstronomyShow(models.Model):
-    title = models.CharField(max_length=63, blank=True)
-    description = models.TextField(max_length=255)
-    themes = models.ManyToManyField("ShowTheme", related_name="astronomy_shows")
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    themes = models.ManyToManyField(ShowTheme, blank=True)
+    image = models.ImageField(null=True, upload_to=astronomy_show_image_file_path)
+
+    class Meta:
+        ordering = ["title"]
 
     def __str__(self):
         return self.title
 
 
-class ShowTheme(models.Model):
-    name = models.CharField(max_length=63, blank=True)
-
-    def __str__(self):
-        return self.name
-
-
-class PlanetariumDome(models.Model):
-    name = models.CharField(max_length=63)
-    rows = models.IntegerField()
-    seats_in_row = models.IntegerField()
-
-    def __str__(self):
-        return self.name
-
-
 class ShowSession(models.Model):
-    astronomy_show = models.ForeignKey(AstronomyShow, on_delete=models.CASCADE, related_name="sessions")
-    planetarium_dome = models.ForeignKey(PlanetariumDome, on_delete=models.CASCADE, related_name="sessions")
     show_time = models.DateTimeField()
+    astronomy_show = models.ForeignKey(AstronomyShow, on_delete=models.CASCADE)
+    planetarium_dome = models.ForeignKey(PlanetariumDome, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["-show_time"]
 
     def __str__(self):
-        return f"{self.astronomy_show} at {self.show_time}"
+        return f"{self.astronomy_show.title} at {self.show_time}"
 
 
 class Reservation(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reservations")
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
 
     def __str__(self):
-        return f"Reservation by {self.user} at {self.created_at}"
+        return f"Reservation by {self.user.username} at {self.created_at}"
+
+    class Meta:
+        ordering = ["-created_at"]
 
 
 class Ticket(models.Model):
+    show_session = models.ForeignKey(
+        ShowSession, on_delete=models.CASCADE, related_name="tickets"
+    )
+    reservation = models.ForeignKey(
+        Reservation, on_delete=models.CASCADE, related_name="tickets"
+    )
     row = models.IntegerField()
     seat = models.IntegerField()
-    show_session = models.ForeignKey(ShowSession, on_delete=models.CASCADE, related_name="tickets")
-    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE, related_name="tickets")
+
+    @staticmethod
+    def validate_ticket(row, seat, planetarium_dome, error_to_raise):
+        for ticket_attr_value, ticket_attr_name, dome_attr_name in [
+            (row, "row", "rows"),
+            (seat, "seat", "seats_in_row"),
+        ]:
+            count_attrs = getattr(planetarium_dome, dome_attr_name)
+            if not (1 <= ticket_attr_value <= count_attrs):
+                raise error_to_raise(
+                    {
+                        ticket_attr_name: f"{ticket_attr_name} "
+                        f"number must be in available range: "
+                        f"(1, {dome_attr_name}): "
+                        f"(1, {count_attrs})"
+                    }
+                )
+
+    def clean(self):
+        Ticket.validate_ticket(
+            self.row,
+            self.seat,
+            self.show_session.planetarium_dome,
+            ValidationError,
+        )
+
+
+    def save(
+        self,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None,
+    ):
+        self.full_clean()
+        return super(Ticket, self).save(
+            force_insert, force_update, using, update_fields
+        )
 
     def __str__(self):
-        return f"Ticket {self.row}-{self.seat} for {self.show_session}"
+        return (
+            f"{str(self.show_session)} (row: {self.row}, seat: {self.seat})"
+        )
+
+    class Meta:
+        unique_together = ("show_session", "row", "seat")
+        ordering = ["row", "seat"]
