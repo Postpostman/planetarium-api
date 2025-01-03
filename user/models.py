8138b99b9dd2b631f53128aedd82ea0d18d16b00
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2025-01-03 16:36:47 +0200

    add groups and permissions for user

--- user/models.py
+++ user/models.py
@@ -3,4 +3,17 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    groups = models.ManyToManyField(
+        "auth.Group",
+        related_name="custom_user_set",
+        blank=True,
+        help_text="The groups this user belongs to.",
+        verbose_name="groups",
+    )
+    user_permissions = models.ManyToManyField(
+        "auth.Permission",
+        related_name="custom_user_set",
+        blank=True,
+        help_text="Specific permissions for this user.",
+        verbose_name="user permissions",
+    )
