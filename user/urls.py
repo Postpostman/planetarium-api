c8f3b9c347e6e00fcdb29aa7b1217861847bac23
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2025-01-06 23:09:36 +0200

    add tokens path to user urlpatterns

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
-
+from .views import UserViewSet, CreateTokenView, ManageUserView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 router = DefaultRouter()
 router.register(r"users", UserViewSet, basename="user")
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("register/", CreateTokenView.as_view(), name="create"),
+    path("me/", ManageUserView.as_view(), name="manage"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
+
+app_name = "user"
