1e4fee1d9fe233fb900eea26c4004e2b98b91d9e
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2025-01-07 16:42:16 +0200

    add dotenv to settings and update permissions by using safemethods

--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,10 @@ from pathlib import Path
 import os
 from datetime import timedelta
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,13 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-n-x_(4pe)hb-1)qsw(1l#*f@6=*h8ije$fy1o(@30ov7baacew"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost',
-                 '127.0.0.1', ]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
 
 # Application definition
 
--- planetarium/permissions.py
+++ planetarium/permissions.py
@@ -1,8 +1,28 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
-class IsOwnerOrReadOnly(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in ("GET", "HEAD", "OPTIONS"):
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
             return True
-        return obj.user == request.user
+        return request.user and request.user.is_staff
+
+
+class IsAuthorizedOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return request.user and request.user.is_authenticated
+        return request.user and request.user.is_staff
+
+
+class IsAuthorized(BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
+
+class IsAdminOrAuthorized(BasePermission):
+    def has_permission(self, request, view):
+        return request.user and (
+                request.user.is_staff
+                or request.user.is_authenticated
+        )
